c5650aad3c2f2055d67a1baba74738be38195ba0
GuillermoCairo26 <guillermocairo14@gmail.com>
2021-06-16 00:03:17 +0000

    sin terminar

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
-// app.js file content
-console.log("Hello World")
+// this is my function that converts dollars to yen
+const fromDollarToYen = (dollar) => {
+    return (dollar * 106.5); 
+}
+
+// this is my function that converts euro to dollar
+const fromEuroToDollar = (Euro) => {
+    return (Euro * 1.2);
+}
+
+// this is my function that converts yen to pound
+const fromYenToPound = (Yen) => {
+    return (Yen * 0.0067);
+}
+
+module.exports = {  fromEuroToDollar, fromDollarToYen, fromYenToPound }
